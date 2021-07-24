0baafd5fef57446f516b75816af49fb6f2112149
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-24 18:57:32 -0300

    Navbar - incomplete

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Router, Switch, Route, Redirect  } from 'react-router-dom';
+import Navbar from './core/components/Navbar';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import MedicoDetails from 'pages/Catalog/components/MedicoDetails';
+import Auth from 'pages/Auth';
+import Admin from './pages/Admin';
+import Report from './pages/Report';
+import history from './core/utils/history';
+import DashBoard from 'pages/DashBoard';
+
+const Routes = () => (
+    <Router history={history}>
+    <Navbar />
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/medicos"  exact>
+                <Catalog />
+            </Route>
+            <Route path="/medicos/:medicoId">
+                <MedicoDetails />
+            </Route>
+            <Redirect from="/auth" to="/auth/login" exact/>
+            <Route path="/auth">
+                <Auth />
+            </Route>
+            <Redirect from="/report" to="/report/medicos" exact/>
+            <Route path="/report">
+                <Report />
+            </Route>
+            <Redirect from="/dashboard" to="/dashboard/servicos" exact/>
+            <Route path="/dashboard">
+                <DashBoard />
+            </Route>
+            <Redirect from="/admin" to="/admin/medicos" exact/>
+            <Route path="/admin">
+                <Admin />
+            </Route>
+        </Switch>
+    </Router>
+);
+
+export default Routes;
---
+++ frontend/src/core/components/Navbar/index.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react';
+import { getAccessTokenDecoded, logout, isAllowebByRole } from 'core/utils/auth';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import menu from 'core/assets/images/menu-icon.svg';
+import logo from 'core/assets/images/navbar-logo.jpg';
+import './styles.scss';
+
+const Navbar = () => {
+    const [drawerActive, setDrawerActive] = useState(false);
+    const [currentUser, setCurrentUser] = useState('');
+    const location = useLocation();
+
+    const handleLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+        logout(); 
+    }
+
+    useEffect(() => {
+        const currentUserData = getAccessTokenDecoded();
+        setCurrentUser(currentUserData.user_name);
+    }, [location]);
+    
+    return (
+        <nav className="bg-primary navbar-main">
+  
+                <Link to="/" className="navbar-logo-text">
+                    <img 
+                    src={logo} 
+                    alt="Foto"
+                    className="navbar-logo" />
+                    <h4>MapinPedagogics</h4>
+                </Link>
+                <button className="navbar-mobile-btn" type="button" onClick={() => setDrawerActive(!drawerActive)}>
+                    <img src={menu} alt="Mobile Menu" />
+                </button>
+
+                <div className={ drawerActive ? "navbar-mobile-container" : "navbar-menu-container" }> 
+                <ul className="navbar-main-menu">
+                    <li>
+                    <NavLink onClick={() => setDrawerActive(false)} className="navbar-link" to="/" activeClassName="active" exact>HOME</NavLink>
+                    </li>
+                    <li>
+                    <NavLink onClick={() => setDrawerActive(false)} className="navbar-link" to="/medicos" activeClassName="active" exact>LISTA</NavLink>
+                    </li>
+
+                    {isAllowebByRole(['ROLE_ADMIN']) && (
+                        <li>
+                        <NavLink onClick={() => setDrawerActive(false)} className="navbar-link" to="/report" activeClassName="active" exact>RELATÓRIOS</NavLink>
+                    </li>
+                    )}
+                    
+                    {isAllowebByRole(['ROLE_ADMIN']) && (
+                        <li>
+                        <NavLink onClick={() => setDrawerActive(false)} className="navbar-link" to="/admin" activeClassName="active" exact>ADMIN</NavLink>
+                    </li>
+                    )}
+                    
+                    {drawerActive && (
+                            <li>
+                                {currentUser && (
+                                    <a 
+                                    href="#logout" 
+                                    className="nav-link active d-inline" 
+                                    onClick={(e) => {
+                                        setDrawerActive(false);
+                                        handleLogout(e);
+                                    }} >
+                                        {`LOGOUT - ${currentUser}`}
+                                    </a>
+                                )}
+                            </li>
+                    )}
+
+                    {drawerActive && (
+                            <>
+                                {!currentUser && (
+                                    <li>
+                                        <Link onClick={() => setDrawerActive(false)} to="/auth/login" className="navbar-link active">LOGIN</Link>
+                                    </li>        
+                                )}
+                            </>
+                        )}
+                </ul>
+            </div> 
+            <div className="user-info-dnone text-right">
+               { currentUser && (
+                   <>
+                    <a href="logout" className="navbar-link active d-inline ml-3" onClick={(e) => {
+                        setDrawerActive(false);
+                        handleLogout(e);
+                    }}>
+                        LOGOUT
+                    </a>
+                    <div>  
+                        <h6 className="nav-link-email">{currentUser}</h6>
+                    </div>                   
+                   </>
+               )}
+                { !currentUser && (
+                    <Link onClick={() => setDrawerActive(false)} className="navbar-link active" to="/auth/login">
+                    LOGIN
+                    </Link>
+                )}
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
