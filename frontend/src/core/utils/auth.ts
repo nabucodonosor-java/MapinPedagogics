dba20f37c2b9601ebeee413fb0df1da3ac3bf6c2
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-25 15:47:06 -0300

    Add utils

---
+++ frontend/src/core/utils/auth.ts
@@ -0,0 +1,73 @@
+import jwtDecode from 'jwt-decode';
+import history from './history';
+
+export const CLIENT_ID = 'pedagogics';
+export const CLIENT_SECRET = 'pedagogics123';
+
+type LoginResponse = {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    scope: string;
+    userFirstName: string;
+    userId: number;
+}
+
+export type Role = 'ROLE_STUDENT' | 'ROLE_INSTRUCTOR' | 'ROLE_ADMIN';
+
+type AccessToken = {
+    exp: number;
+    user_name: string;
+    authorities: Role[];
+}
+
+export const saveSessionData = (loginResponse: LoginResponse) => {
+    localStorage.setItem('authData', JSON.stringify(loginResponse));
+}
+
+export const getSessionData = () => {
+    const sessionData = localStorage.getItem('authData') ?? '{}';
+    const parsedSessionData = JSON.parse(sessionData);
+
+    return parsedSessionData as LoginResponse;
+}
+
+export const getAccessTokenDecoded = () => {
+    const sessionData = getSessionData();
+
+    try {
+
+        const tokenDecoded = jwtDecode(sessionData.access_token);
+        return tokenDecoded as AccessToken;
+
+    } catch (error) {
+        return {} as AccessToken;
+    }
+}
+
+export const isTokenValid = () => {
+    const { exp } = getAccessTokenDecoded();
+
+    return Date.now() <= exp * 1000;
+}
+
+export const isAuthenticated = () => {
+    const sessionData = getSessionData();
+
+    return sessionData.access_token && isTokenValid();
+}
+
+export const isAllowedByRole = (routeRoles: Role[] = []) => {
+    if (routeRoles.length === 0) {
+        return true;
+    }
+
+    const { authorities } = getAccessTokenDecoded();
+
+    return routeRoles.some(role => authorities?.includes(role));
+}
+
+export const logout = () => {
+    localStorage.removeItem('authData');
+    history.replace('auth/login');
+}
